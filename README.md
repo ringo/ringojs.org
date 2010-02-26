431a8981366c77850107f4a41f3e888dbafe2e8f
Hannes Wallnöfer <hannesw@gmail.com>
2010-02-26 13:36:21 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This is the application running on <http://ringojs.org/>.
+
+Steps to run this app locally create symbolic links to the 
+demo, jsdoc and ringowiki apps in this directory. Then run the 
+following command:
+
+    ringo ringo/webapp .
+
+Note that at this app may require the current master branch from 
+<http://github.com/hns/ringojs> to work.
---
+++ config.js
@@ -0,0 +1,25 @@
+exports.app = require('ringo/webapp').handleRequest;
+
+exports.middleware = [
+    'ringo/middleware/gzip',
+    'ringo/middleware/etag',
+    'ringo/middleware/responselog',
+    'ringo/middleware/error',
+    'ringo/middleware/notfound'
+    // 'ringo/middleware/profiler'
+];
+
+exports.urls = [
+    [ '^$', function() { throw { redirect: '/wiki'}; } ],
+    [ '^demo', './demo/config' ],
+    [ '^api', './jsdoc/config' ],
+    [ '^wiki', './ringowiki/config' ]
+];
+
+exports.static = [
+    [ '/demo/static', './demo/static' ], 
+    [ '/api/static', './jsdoc/static' ],
+    [ '/wiki/static', './ringowiki/static' ],
+    [ '/static', './static' ]
+];
+
