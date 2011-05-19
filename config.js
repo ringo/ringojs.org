aa725bb20d08c20d0fc9bd6a6902fb776562d8da
Hannes Wallnöfer <hannesw@gmail.com>
2011-05-19 19:41:24 +0200

    Activate ircbot.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var {Application} = require("stick");
 var app = exports.app = Application();
 app.configure("gzip", "etag", masterTemplate, "mount");
 app.mount("/wiki", module.resolve("ringowiki/config"));
-// app.mount("/bot", module.resolve("ringobot/config"));
+app.mount("/bot", module.resolve("ringobot/config"));
 app.mount("", module.resolve("simplesite/config"));
 
 var masterTemplatePath = module.resolve("templates/master.html");
@@ -12,6 +12,14 @@ function masterTemplate(next, app) {
         req.env.masterTemplate = masterTemplatePath;
         return next(req);
     }
+}
+
+exports.start = function(server) {
+    require("./ringobot/config").start(server);
+}
+
+exports.stop = function(server) {
+    require("./ringobot/config").stop(server);
 }
 
 exports.webHookConfig = {
